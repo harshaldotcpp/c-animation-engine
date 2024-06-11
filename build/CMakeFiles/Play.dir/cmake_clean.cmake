1a200e94ddd55440d4ba91cb2d91fff93f0c4362
Harshal <harshalmahapure44@gmail.com>
2024-06-11 18:37:24 +0530

    2

---
+++ build/CMakeFiles/Play.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Play.dir/main.cpp.obj"
+  "CMakeFiles/Play.dir/main.cpp.obj.d"
+  "Play.exe"
+  "Play.exe.manifest"
+  "Play.pdb"
+  "libPlay.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Play.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Play.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Play.
+# This may be replaced when dependencies are built.
